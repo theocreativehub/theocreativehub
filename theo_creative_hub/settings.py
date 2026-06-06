24f61a29881b387d53899292653cab1c13b134d5
theocreativehub <theophilusanimbioh@gmail.com>
2026-06-06 01:59:34 +0000

    Add whitenoise and enable debug

--- theo_creative_hub/settings.py
+++ theo_creative_hub/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k=0mr!_banq$sruor2d%k5yv(n%$!e@@n-!6@ycl*hyxbv1z3)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
